9bf090de6cbe0d64d72ff2b684b42ade12d88fc7
tikabureiku <chta24017@g.nihon-u.ac.jp>
2026-01-08 17:20:39 +0900

    コミット

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,9 @@ function HomePage() {
       <Button variant="contained" onClick={() => navigate("/verse")}>
         各チームと選手一覧
       </Button>
+      <Button variant="contained" onClick={()=> navigate("/players")}>
+        チーム順位
+      </Button>
     </>
   );
 }
@@ -49,6 +52,37 @@ function VersePage() {
     </>
   );
 }
+
+function PlayersPage() {
+  return (
+    <>
+      <h1>各チームと選手</h1>
+
+      <ul>
+        <li>
+          角川サクラナイツ
+          <ul>
+            <li>岡田紗佳</li>
+            <li>堀慎吾</li>
+            <li>渋川難波</li>
+            <li>内川幸太郎</li>
+          </ul>
+        </li>
+
+        <li>
+          KONAMI麻雀格闘倶楽部
+          <ul>
+            <li>佐々木寿人</li>
+            <li>高宮まり</li>
+            <li>伊達朱里紗</li>
+            <li>滝沢和典</li>
+          </ul>
+        </li>
+      </ul>
+    </>
+  );
+}
+
 
 export default function App() {
   return (
